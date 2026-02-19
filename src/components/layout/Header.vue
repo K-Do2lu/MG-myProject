73afa9bdbc4701e1b88d6ec0d38719fa501df850
doyeon <kdy992m@naver.com>
2026-02-19 17:06:49 +0900

    add: Header nav, mo nav

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,91 +1,104 @@
 <template>
   <header id="header" class="header">
     <div class="header-cont">
+
       <h1 class="logo">
         <a href="https://www.mgdatasystem.co.kr/">
           <img src="@/assets/images/logo_mgds.png" alt="MG데이터시스템">
         </a>
       </h1>
 
-      <nav>
+      <!-- 햄버거 버튼 -->
+      <button
+        class="btn-menu"
+        @click="toggleMenu"
+        :class="{ 'is-open': isMobileMenu }"
+        :aria-expanded="isMobileMenu"
+        aria-label="메뉴 열기">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <!-- 네비 -->
+      <nav
+        class="gnb"
+        :class="{ 'is-mobile-open': isMobileMenu }">
+
         <ul>
-          <li>
-            <router-link to="/sub1">COMPANY</router-link>
+          <li v-for="(menu, idx) in menuData" :key="idx">
+
+            <router-link
+              :to="menu.link"
+              class="depth1">
+              {{ menu.title }}
+            </router-link>
+
             <ul class="sub-menu">
-              <li><router-link to="/sub1/sub1_1">회사개요</router-link></li>
-              <li><router-link to="/sub1/sub1_2">CEO인사말</router-link></li>
-              <li><router-link to="/sub1/sub1_3">연혁</router-link></li>
-              <li><router-link to="/sub1/sub1_4">오시는 길</router-link></li>
-              <li><router-link to="/sub1/sub1_5">협력사 소개</router-link></li>
+              <li v-for="(sub, sIdx) in menu.subs" :key="sIdx">
+                <router-link :to="sub.link">
+                  {{ sub.name }}
+                </router-link>
+              </li>
             </ul>
+
           </li>
-          <li>
-            <router-link to="/sub2">BUSINESS</router-link>
-            <ul class="sub-menu">
-              <li><router-link to="/sub2/sub2_1">사업영역</router-link></li>
-            </ul>
+
+          <li class="logo-dshop">
+            <a href="https://mgdshop.co.kr/main/index.do" target="_blank">
+              <img src="@/assets/images/logo_dshop.png" alt="MGDshop">
+            </a>
           </li>
-          <li>
-            <router-link to="/sub3">NOTICE</router-link>
-            <ul class="sub-menu">
-              <li><router-link to="/sub3/sub3_1">공지사항</router-link></li>
-              <li><router-link to="/sub3/sub3_2">입찰공고</router-link></li>
-              <li><router-link to="/sub3/sub3_3">일정관리</router-link></li>
-            </ul>
-          </li>
-          <li>
-            <router-link to="/sub4">RECRUIT</router-link>
-            <ul class="sub-menu">
-              <li><router-link to="/sub3/sub4_1">채용공고</router-link></li>
-              <li><router-link to="/sub3/sub4_2">인재상</router-link></li>
-              <li><router-link to="/sub3/sub4_3">자료실</router-link></li>
-            </ul>
-          </li>
-          <li class="logo-dshop"><a href="https://mgdshop.co.kr/main/index.do" title="새창 열림"  target="_blank"><img src="@/assets/images/logo_dshop.png" alt="MGDshop"></a></li>
+
         </ul>
       </nav>
+
+      <!-- 모바일 배경 -->
+      <div
+        class="overlay"
+        v-if="isMobileMenu"
+        @click="toggleMenu">
+      </div>
+
     </div>
   </header>
 </template>
 
-   <!-- <button
-      type="button"
-      class="btn-menu"
-      aria-controls="gnb"
-      :aria-expanded="isOpen.toString()"
-      @click="toggle"
-    >
-      메뉴
-    </button>
-
-    <nav id="gnb" class="gnb" :hidden="!isOpen">
-      <ul>
-        <li><a href="#">Menu 1</a></li>
-        <li><a href="#">Menu 2</a></li>
-      </ul>
-    </nav> -->
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref } from "vue";
 
-const isOpen = ref(false);
-const toggle = () => {
-  isOpen.value = !isOpen.value;
-};
+const isHover = ref(false);
 
-const onKeydown = (e) => {
-  if (e.key === "Escape" && isOpen.value) {
-    isOpen.value = false;
-  }
-};
+// 유지보수용 데이터: 메뉴가 추가되어도 이 객체만 수정하면 됨
+const menuData = [
+  { title: "COMPANY", link: "/sub1", subs: [
+    { name: "회사개요", link: "/sub1/sub1_1" },
+    { name: "CEO인사말", link: "/sub1/sub1_2" },
+    { name: "연혁", link: "/sub1/sub1_3" },
+    { name: "오시는 길", link: "/sub1/sub1_4" },
+    { name: "협력사 소개", link: "/sub1/sub1_5" }
+  ]},
+  { title: "BUSINESS", link: "/sub2", subs: [{ name: "사업영역", link: "/sub2_1" }] },
+  { title: "NOTICE", link: "/sub3", subs: [
+    { name: "공지사항", link: "/sub3_1" },
+    { name: "입찰공고", link: "/sub3_2" },
+    { name: "일정관리", link: "/sub3_3" }
+  ]},
+  { title: "RECRUIT", link: "/sub4", subs: [
+    { name: "채용공고", link: "/sub4_1" },
+    { name: "인재상", link: "/sub4_2" },
+    { name: "자료실", link: "/sub4_3" }
+  ]}
+];
 
-// 컴포넌트 살아 있을 때만 이벤트 등록
-onMounted(() => {
-  window.addEventListener("keydown", onKeydown);
-});
+// 햄버거
+const isMobileMenu = ref(false);
 
-// 페이진 전환 시 이벤트 누수 방지
-onBeforeUnmount(() => {
-  window.removeEventListener("keydown", onKeydown);
-});
+const toggleMenu = () => {
+  isMobileMenu.value = !isMobileMenu.value;
+
+  // 모바일 메뉴 열릴 때 스크롤 방지
+  document.body.style.overflow = isMobileMenu.value ? "hidden" : "";
+}
 </script>
